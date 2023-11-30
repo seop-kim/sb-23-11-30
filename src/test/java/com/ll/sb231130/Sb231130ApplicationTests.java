4a497f685dad2f5fd54a5c39c38c7e6ae3d0df41
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-30 13:10:53 +0900

    Initial commit

---
+++ src/test/java/com/ll/sb231130/Sb231130ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231130;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231130ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
